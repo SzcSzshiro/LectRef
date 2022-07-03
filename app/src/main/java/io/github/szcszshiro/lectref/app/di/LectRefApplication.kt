179a3bfa35346be08964019135ec5e4d62f161ce
SzcSzshiro <szcszshiro@gmail.com>
2022-07-03 21:23:52 +0900

    [import]Hiltを追加
    HIltを依存として追加

---
+++ app/src/main/java/io/github/szcszshiro/lectref/app/di/LectRefApplication.kt
@@ -0,0 +1,8 @@
+package io.github.szcszshiro.lectref.app.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class LectRefApplication: Application() {
+}

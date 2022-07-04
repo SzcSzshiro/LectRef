e15f57c6df81062858e84284a8ed922e9bfc659b
SzcSzshiro <szcszshiro@gmail.com>
2022-07-05 02:42:34 +0900

    [update]講義集約を実装しました
    講義集約のLectureとTaskを実装しました。

--- app/src/main/java/io/github/szcszshiro/lectref/domain/task/Task.kt
+++ app/src/main/java/io/github/szcszshiro/lectref/domain/task/Task.kt
@@ -1,4 +1,42 @@
 package io.github.szcszshiro.lectref.domain.task
 
-class Task {
+import io.github.szcszshiro.lectref.domain.lecture.Lecture
+import java.time.LocalDateTime
+
+class Task private constructor(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val deadLine: LocalDateTime,
+    val isDone: Boolean
+){
+    companion object{
+        fun reconstruct(
+            id: Int,
+            name: String,
+            description: String,
+            deadLine: LocalDateTime,
+            isDone: Boolean
+        ) = Task(id, name, description, deadLine, isDone)
+
+        fun create(
+            id: Int,
+            name: String,
+            description: String,
+            deadLine: LocalDateTime,
+            isDone: Boolean
+        ): Task?{
+            if (!Lecture.isNameOk(name) || !Lecture.isDescriptionOk(description)){
+                return null
+            }
+            return Task(id, name, description, deadLine, isDone)
+        }
+
+        fun isNameOk(name: String) =
+            name.length in 3..15
+        fun isDescriptionOk(description: String) =
+            description.length in 0..50
+    }
+
+
 }

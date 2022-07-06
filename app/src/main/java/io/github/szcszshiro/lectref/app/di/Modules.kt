903c7bb51e2b3b79ee9d6a9b56f5a07b83078d73
SzcSzshiro <szcszshiro@gmail.com>
2022-07-07 02:13:00 +0900

    [add]DBのDIモジュールを追加

--- app/src/main/java/io/github/szcszshiro/lectref/app/di/Modules.kt
+++ app/src/main/java/io/github/szcszshiro/lectref/app/di/Modules.kt
@@ -1,2 +1,29 @@
 package io.github.szcszshiro.lectref.app.di
 
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import io.github.szcszshiro.lectref.app.db.LectRefDB
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LectRefDBModule{
+    @Singleton
+    @Provides
+    fun provideDB(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, LectRefDB::class.java, "lifeline_todo_db").build()
+
+    @Provides
+    fun provideLectureDao(db: LectRefDB) = db.getLectureDao()
+
+    @Provides
+    fun provideReferenceDao(db: LectRefDB) = db.getReferenceDao()
+
+    @Provides
+    fun provideTaskDao(db: LectRefDB) = db.getTaskDao()
+}

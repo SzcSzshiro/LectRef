fc8c9809bc98fb9e1b591e722904d9c1fd62ce30
SzcSzshiro <szcszshiro@gmail.com>
2022-07-07 03:09:14 +0900

    [add]ユースケースを追加
    講義保存ユースケースを追加

--- app/src/main/java/io/github/szcszshiro/lectref/usecase/RecordLectureUseCase.kt
+++ app/src/main/java/io/github/szcszshiro/lectref/usecase/RecordLectureUseCase.kt
@@ -1,4 +1,48 @@
 package io.github.szcszshiro.lectref.usecase
 
-class RecordLectureUseCase {
+import io.github.szcszshiro.lectref.domain.lecture.Lecture
+import io.github.szcszshiro.lectref.infrastructure.LectureRepository
+import io.github.szcszshiro.lectref.infrastructure.ReferenceRepository
+import io.github.szcszshiro.lectref.infrastructure.TaskRepository
+import kotlinx.coroutines.runBlocking
+import java.lang.IllegalArgumentException
+import java.time.DayOfWeek
+import java.time.LocalTime
+import javax.inject.Inject
+
+class RecordLectureUseCase @Inject constructor(
+    private val lectureRepository: LectureRepository,
+    private val referenceRepository: ReferenceRepository,
+    private val taskRepository: TaskRepository
+){
+    @Throws(IllegalArgumentException::class)
+    fun addLecture(
+        id: Int,
+        name: String,
+        description: String,
+        week: DayOfWeek,
+        startTime: LocalTime
+    ){
+        require(Lecture.isNameOk(name) && Lecture.isDescriptionOk(description))
+        val newLecture = Lecture.create(id, name, description, week, startTime)?: return
+        runBlocking {
+            lectureRepository.add(newLecture)
+        }
+    }
+
+    fun removeLecture(id: Int){
+        runBlocking {
+            val target = lectureRepository.findFromId(id) ?: return@runBlocking
+            val targetReferences = referenceRepository.findFromLectureId(id)
+            val targetTasks = taskRepository.findFromLectureId(id)
+
+            targetReferences.forEach {
+                referenceRepository.remove(it)
+            }
+            targetTasks.forEach {
+                taskRepository.remove(it)
+            }
+            lectureRepository.remove(target)
+        }
+    }
 }

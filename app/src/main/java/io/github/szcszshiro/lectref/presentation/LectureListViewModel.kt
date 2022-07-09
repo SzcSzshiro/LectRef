9f4e34380c8fdefce3bfbdae4d7d713fed64e0c3
SzcSzshiro <szcszshiro@gmail.com>
2022-07-09 10:29:28 +0900

    [update]ViewModelを更新

--- app/src/main/java/io/github/szcszshiro/lectref/presentation/LectureListViewModel.kt
+++ app/src/main/java/io/github/szcszshiro/lectref/presentation/LectureListViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.szcszshiro.lectref.usecase.ObserveLectureUseCase
+import io.github.szcszshiro.lectref.usecase.ObserveReferenceUseCase
 import io.github.szcszshiro.lectref.usecase.ObserveTaskUseCase
+import io.github.szcszshiro.lectref.usecase.RecordLectureUseCase
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -12,18 +14,11 @@ import javax.inject.Inject
 @HiltViewModel
 class LectureListViewModel @Inject constructor(
     private val observeLectureUseCase: ObserveLectureUseCase,
-    private val observeTaskUseCase: ObserveTaskUseCase
+    private val observeTaskUseCase: ObserveTaskUseCase,
 ): ViewModel() {
-    fun getLectureLivedata() = observeLectureUseCase.getFlow().asLiveData()
+    fun getLectureLivedata() =
+        observeLectureUseCase.getFlow().asLiveData()
 
-    fun getTasksLivedata(lectureId: Int? = null) =
-        observeTaskUseCase.getFlow().map { list ->
-            if (lectureId != null) {
-                return@map list.filter {
-                    it.lectureId == lectureId
-                }
-            } else {
-                return@map list
-            }
-        }.asLiveData()
+    fun getTaskLivedata() =
+        observeTaskUseCase.getFlow().asLiveData()
 }

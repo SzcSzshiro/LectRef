d9223df325e3e099f12aad68d2167e85528d3c47
SzcSzshiro <szcszshiro@gmail.com>
2022-07-10 16:09:16 +0900

    [update]UIを更新
    講義カードUI関係を更新しました

--- app/src/main/java/io/github/szcszshiro/lectref/app/MainActivity.kt
+++ app/src/main/java/io/github/szcszshiro/lectref/app/MainActivity.kt
@@ -1,38 +1,24 @@
 package io.github.szcszshiro.lectref.app
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import io.github.szcszshiro.lectref.app.ui.molecules.LectureCard
 import io.github.szcszshiro.lectref.app.ui.theme.LectRefTheme
 import io.github.szcszshiro.lectref.presentation.LectureListViewModel
-import io.github.szcszshiro.lectref.usecase.RecordLectureUseCase
-import io.github.szcszshiro.lectref.usecase.RecordReferenceUseCase
-import io.github.szcszshiro.lectref.usecase.RecordTaskUseCase
-import java.time.DayOfWeek
-import java.time.LocalDateTime
-import java.time.LocalTime
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -50,7 +36,33 @@ class MainActivity : ComponentActivity() {
                         startDestination = "lecture_list"
                     ){
                         composable("lecture_list"){
-
+                            val vm: LectureListViewModel by viewModels()
+                            val lectures = vm.getLectureLivedata().observeAsState()
+                            val tasks = vm.getTaskLivedata().observeAsState()
+                            Scaffold(
+                                topBar = {
+                                    TopAppBar(
+                                        title = { Text(text = "LectRef")}
+                                    )
+                                }
+                            ) {
+                                LazyColumn {
+                                    items(lectures.value?: emptyList()){ lecture ->
+                                        val taskNum = tasks.value?.filter {
+                                            it.lectureId == lecture.id
+                                        }?.size ?: 0
+                                        LectureCard(
+                                            lectureName = lecture.name,
+                                            taskNum = taskNum,
+                                            week = lecture.week.toString(),
+                                            startTime = lecture.startTime,
+                                            onClickCard = { /*TODO*/ },
+                                            onClickTask = { /*TODO*/ },
+                                            modifier = Modifier.padding(8.dp)
+                                        )
+                                    }
+                                }
+                            }
                         }
 
                         composable("lecture_detail"){
--- app/src/main/java/io/github/szcszshiro/lectref/app/ui/molecules/LectureCard.kt
+++ app/src/main/java/io/github/szcszshiro/lectref/app/ui/molecules/LectureCard.kt
@@ -1,16 +1,18 @@
 package io.github.szcszshiro.lectref.app.ui.molecules
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import io.github.szcszshiro.lectref.app.ui.atoms.TaskNumber
 import java.time.LocalTime
 
 @Composable
@@ -19,56 +21,63 @@ fun LectureCard(
     taskNum: Int,
     week: String,
     startTime: LocalTime,
-    onClick: () -> Unit,
+    onClickCard: () -> Unit,
     onClickTask: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Button(
-        onClick = onClick,
-        colors = ButtonDefaults.outlinedButtonColors(
-            backgroundColor = Color.DarkGray
-        ),
-        modifier = modifier.height(120.dp)
+    Card(
+        elevation = 2.dp,
+        modifier = modifier
     ) {
-        Column(modifier = Modifier.fillMaxSize()){
-            Spacer(modifier = Modifier.weight(0.2f))
+        Column(modifier = Modifier.clickable { onClickCard() }) {
             Text(
                 text = lectureName,
-                fontSize = 29.sp,
-                color = Color.White,
+                fontSize = 25.sp,
                 overflow = TextOverflow.Ellipsis,
-                maxLines = 1,
-                modifier = Modifier.weight(1.5f)
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
             )
-            Spacer(modifier = Modifier.weight(0.2f))
-            Row(modifier = Modifier.weight(1.0f)) {
-                TaskNumber(
-                    taskNum = taskNum,
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(start = 8.dp, end = 8.dp)
+                    .fillMaxWidth()
+            ) {
+                TextButton(
                     onClick = onClickTask,
-                    modifier = Modifier.weight(3f)
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(text = "Task: $taskNum")
+                }
+                Text(
+                    text = week,
+                    textAlign = TextAlign.End,
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp)
+                        .weight(1f)
                 )
-                Spacer(modifier = Modifier.weight(1f))
-                WeekAndTime(
-                    weekWord = week,
-                    weekColor = Color.Blue,
-                    time = startTime,
-                    height = 20,
-                    modifier = Modifier.weight(5f)
+                Text(
+                    text = "${startTime.hour}:${startTime.minute}~",
+                    modifier = Modifier.weight(1f)
                 )
             }
         }
     }
 }
 
-@Preview(device = Devices.PIXEL_3)
+@Preview
 @Composable
 fun LectureCardPreview() {
     LectureCard(
-        lectureName = "電気電子情報系特論あいうえお",
-        taskNum = 3,
-        week = "月",
+        lectureName = "Test Lecture",
+        taskNum = 1,
+        week = "Mon",
         startTime = LocalTime.now(),
-        onClick = {},
-        onClickTask = {}
+        onClickCard = {},
+        onClickTask = {},
+        modifier = Modifier
+            .width(300.dp)
+            .wrapContentHeight()
     )
 }
